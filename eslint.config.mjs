ccaa5d6ba0e16db04a6126691615c2c0633a3b61
Edson Frainlar <fedson@kaartech.com>
2025-01-28 01:04:12 +0530

    chore: added prisma

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["commitlint.config.js"], // Exclude commitlint.config.js from ESLint
+  },
 ];
 
 export default eslintConfig;
